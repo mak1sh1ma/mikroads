ae9952193efff7aa8342241de5e458eabb3dfcb8
mak1sh1ma <ezerdumale@gmail.com>
2017-03-25 22:49:12 +0800

    Updated Package

--- packageWar.sh
+++ packageWar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown -R ubuntu:ubuntu /home/ubuntu/mikroad_files
 cd /home/ubuntu/mikroad_files
 mvn clean install
 sudo mv target/MikroAds.war /var/lib/tomcat7/webapps/MikroAds.war
